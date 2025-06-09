e7e0d1b94cfef77e2176bc989691a280280c1a71
Nitesh Shah <niteshshah000@gmail.com>
2025-06-09 18:21:12 +0530

    //

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ use App\Http\Controllers\Api\AuthController;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/profile', [AuthController::class, 'profile']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
 Route::get('/blog', [BlockController::class, 'getBlock']);
